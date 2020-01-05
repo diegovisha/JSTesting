9dc4e6418d117a2cb5d5fc7ac0ddf7d5fc8e4f0e
diegovisha <diegojosevilla@outlook.com>
2020-01-05 16:48:06 -0600

    testing-fundamentals: move test and expect to globals

--- 1.TestingFundamentals/index.js
+++ 1.TestingFundamentals/index.js
@@ -1,25 +1,5 @@
 const { sum, substract, sumAsync, substractAsync } = require('./math');
 
-async function test(title, callback) {
-  try {
-    await callback();
-    console.log(`✓ ${title}`);
-  } catch (error) {
-    console.error(`✕ ${title}`);
-    console.error(error);
-  }
-}
-
-function expect(actual) {
-  return {
-    toBe(expected) {
-      if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}`);
-      }
-    }
-  }
-}
-
 // Test: sum()
 
 test("sum adds numbers", () => {
