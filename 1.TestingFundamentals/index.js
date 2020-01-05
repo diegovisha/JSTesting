0ba895d9486f05b1242efaf8b47d19204228a89c
diegovisha <diegojosevilla@outlook.com>
2020-01-05 16:27:10 -0600

    testing-fundamentals: add test function

--- 1.TestingFundamentals/index.js
+++ 1.TestingFundamentals/index.js
@@ -1,28 +1,39 @@
 const { sum, substract } = require('./math');
 
+function test(title, callback) {
+  try {
+    callback();
+    console.log(`✓ ${title}`);
+  } catch (error) {
+    console.error(`✕ ${title}`);
+    console.error(error);
+  }
+}
+
 function expect(actual) {
   return {
     toBe(expected) {
-      if (result !== expected) {
-        throw new Error(`${result} is not equal to ${expected}`);
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`);
       }
     }
   }
 }
 
-let result = 0;
-let expected = 0;
-
 // Test: sum()
 
-result = sum(3, 7);
-expected = 10;
+test("sum adds numbers", () => {
+  const result = sum(3, 7);
+  const expected = 10;
 
-expect(result).toBe(expected);
+  expect(result).toBe(expected);
+});
 
 // Test: substract()
 
-result = substract(7, 3);
-expected = 4;
+test("substract substracts numbers", () => {
+  const result = substract(7, 3);
+  const expected = 4;
 
-expect(result).toBe(expected);
+  expect(result).toBe(expected);
+});
